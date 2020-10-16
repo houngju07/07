fe604ff5416722f53538fbc5ec10c0c071883c62
houngju07 <ohyoungju@sookmyung.ac.kr>
2020-10-16 19:35:48 +0900

    07.03
    07.03

--- main.c
+++ main.c
@@ -3,15 +3,18 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) {
-	int i;
-	
-	for(i=0;i<5;i++)
-	{
-		int temp = 1;
-		printf("temp=%d\n", temp);
-		temp++; 
-	}
+int counter;
 
+void set_counter()
+{
+	counter=20;
+}
+
+int main(int argc, char *argv[]) 
+{	
+	printf("counetr=%d\n", counter);
+	set_counter();
+	printf("counter %d\n", counter);
 	return 0;
 }
+
